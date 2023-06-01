2708dba98231a9e48a25633e1e8470967949c8e7
t1kuni01 <t1kuni01@student.oamk.fi>
2023-06-01 23:01:03 +0530

    first commit

---
+++ src/test/java/com/automation/automation/AutomationApplicationTests.java
@@ -0,0 +1,14 @@
+package com.automation.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.print("Test running");
+    }
+
+}
