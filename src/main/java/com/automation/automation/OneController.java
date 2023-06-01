0f8fb7566c7007723ad158d2cc3180b1fbb655e1
t1kuni01 <t1kuni01@student.oamk.fi>
2023-06-01 23:48:41 +0530

    updated with ng test

---
+++ src/main/java/com/automation/automation/OneController.java
@@ -0,0 +1,20 @@
+package com.automation.automation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OneController {
+
+    @GetMapping(path="/")
+    String hello() {
+        return "Hello World, Spring Boot!";
+    }
+
+
+    @GetMapping(path="/qaautomation")
+    String qaautomation() {
+        return "Hello QA Automation!";
+    }
+
+}
--- src/test/java/com/automation/automation/AutomationApplicationTests.java
+++ src/test/java/com/automation/automation/AutomationApplicationTests.java
@@ -1,14 +1,46 @@
 package com.automation.automation;
 
-import org.junit.jupiter.api.Test;
+import io.restassured.response.ValidatableResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
 
-@SpringBootTest
-class AutomationApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AutomationApplicationTests extends AbstractTestNGSpringContextTests {
+
+    private final static String BASE_URI = "http://localhost:";
+
+    @LocalServerPort
+    private int port;
+
+    @Value("${server.servlet.context-path}")
+    private String basePath;
+
+    private ValidatableResponse response;
 
     @Test
-    void contextLoads() {
-        System.out.print("Test running");
+    public void verifyController1()  {
+        response = given().contentType("application/json")
+                .header("Content-Type", "application/json")
+                .when().get(BASE_URI + port + basePath + "/").then().statusCode(200);
+
+        String Actual = response.extract().asString();
+        System.out.println("Result :"+Actual);
+        Assert.assertEquals("Hello World, Spring Boot!", Actual);
     }
 
+    @Test
+    public void verifyController2()   {
+        response = given().contentType("application/json")
+                .header("Content-Type", "application/json")
+                .when().get(BASE_URI + port + basePath + "/qaautomation").then().statusCode(200);
+
+        String Actual = response.extract().asString();
+        System.out.println("Result :"+Actual);
+        Assert.assertEquals("Hello QA Automation!", Actual);
+    }
 }
